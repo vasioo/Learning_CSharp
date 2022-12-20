ba2952cee3bf8234a4a33453b8a449b9a0ba3600
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:50:53 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace SetsOfElements
+namespace uniqueUsernames
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double[] array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
-            HashSet<double> set1 = new HashSet<double>((int)array[0]);
-            HashSet<double> set2= new HashSet<double>((int)array[1]);
-            HashSet<double> combiningSet= new HashSet<double>();
-            for (double i = 0; i <array[0]; i++)
+            double n = double.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (double i = 0; i < n; i++)
             {
-                set1.Add(double.Parse(Console.ReadLine()));
+                set.Add(Console.ReadLine());
             }
-            for (double i = 0; i < array[1]; i++)
+            foreach (var item in set)
             {
-                set2.Add(double.Parse(Console.ReadLine()));
+                Console.WriteLine(item);
             }
-            foreach (var item in set2)
-            {
-                if (set1.Contains(item))
-                {
-                    combiningSet.Add(item);
-                }
-            }
-            Console.WriteLine(String.Join(" ",combiningSet));
         }
     }
 }
