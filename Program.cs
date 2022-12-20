7763600876083abd2d18097e7e526e25330fd0f9
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:15:56 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-namespace Lambda
+namespace SortingEvenNums
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
-            numbers= numbers.Where(x => x > 3).ToList();
-            //edno i sushto e
-            //numbers= numbers.Where(IsBigger).ToList();
-            //numbers= numbers.Select(Multiply).ToList();
-            numbers= numbers.Where(x=> { return x > 3; }).ToList();
-            Console.WriteLine(String.Join(",",numbers));
-        }
-        public static bool IsBigger(int x) 
-        {
-            return x > 3;
-        }
-        public static int Multiply(int x)
-        {
-            return x * 5;
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            numbers = numbers.Where(x => x % 2 == 0).OrderBy(x=>x).ToArray();
+            Console.WriteLine(String.Join(", ",numbers));
+            
         }
     }
 }
