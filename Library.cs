b71c8175f3ff508894049b22efa09fccbb9b974f
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:09:07 +0200

    Add files via upload

---
+++ Library.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LibraryIterator
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(List<Book> books)
+        {
+            this.books = books;
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ LibraryIterator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LibraryIterator
+{
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books;
+        private int index;
+        public LibraryIterator(List<Book> books)
+        {
+            this.books = books;
+            index = -1;
+        }
+        public Book Current => books[index];
+
+        public bool MoveNext()
+        {
+            index++;
+            return index < books.Count;
+        }
+        public void Reset(){}
+        public void Dispose(){}
+        object IEnumerator.Current => Current;
+    }
+}
