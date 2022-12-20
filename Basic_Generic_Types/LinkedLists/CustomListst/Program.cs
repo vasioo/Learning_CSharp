c64e9d41a474035a42d13f56a52e96857658238e
Vasil <vaskoorakov17@gmail.com>
2022-12-20 18:31:54 +0200

    added custom linked lists

---
+++ Basic_Generic_Types/LinkedLists/CustomListst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList customList = new CustomList();
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            customList.Add(5);
+            customList.Shrink();
+            int element = customList.RemoveAt(2);
+            Console.WriteLine(element);
+            Console.WriteLine(customList.Count);
+            customList.Contains(5);
+            customList.Swap(0, 1);
+        }
+    }
+}
---
+++ Basic_Generic_Types/LinkedLists/Implementing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImplementingLinkedListsAndStacksAndQueues
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DoublyLinkedList linked = new DoublyLinkedList();
+
+            linked.AddFirst(new Node(1));
+            linked.AddFirst(new Node(2));
+            linked.AddFirst(new Node(3));
+
+            linked.AddLast(new Node(1));
+            linked.AddLast(new Node(2));
+            linked.AddLast(new Node(3));
+
+            linked.ForEach(node =>
+            {
+                Console.WriteLine(node.Value);
+            });
+            Console.WriteLine("Reversed: ");
+            linked.Reverse();
+            linked.ForEach(node =>
+            {
+                Console.WriteLine(node.Value);
+            });
+            //foreach (var node in linked.ToArray())
+            //{
+            //    Console.WriteLine("In foreach");
+            //    Console.WriteLine(node.Value);
+            //}
+        }
+    }
+}
