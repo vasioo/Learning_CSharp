e29e6f215ea6b4dd627c2a1f7a341fb66fc9eaa5
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:10:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,71 +1,12 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace IteratorsAndComparators
+namespace Iterators
 {
-    public class Book
+    class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.FirstName = "Peshko";
-            person.LastName = "Peshkov";
-            foreach (var item in person)
-            {
-                Console.WriteLine(item);
-            }
-            //IEnumerator<int> enumerator = person.GetEnumerator();
-            //while (enumerator.MoveNext())
-            //{
-            //    var item = enumerator.Current;
-            //    Console.WriteLine(item);
-            //}
+            Console.WriteLine("Hello World!");
         }
-    }
-    class Person : IEnumerable<int>
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public List<int> Grades { get; set; } = new List<int> { 4, 5, 6 };
-
-        public IEnumerator<int> GetEnumerator()
-        {
-            for (int i = Grades.Count - 1; i >= 0; i--)
-            {
-                yield return Grades[i];
-            }
-            //return Grades.GetEnumerator();
-            //return new ReversePersonGradesEnumeratr(Grades);
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    }
-    public class ReversePersonGradesEnumeratr : IEnumerator<int>
-    {
-        private int index;
-        private List<int> items;
-        public int Current => items[index];
-        public ReversePersonGradesEnumeratr(List<int> items)
-        {
-            this.items = items;
-            index = items.Count;
-        }
-        public bool MoveNext()
-        {
-            index--;
-            if (index < 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        public void Reset() { }
-        public void Dispose() { }
-        object IEnumerator.Current => this.Current;
     }
 }
