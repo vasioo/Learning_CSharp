ddd378660bdc82df0281227ab4ec90835037544b
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:43:36 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,53 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-namespace SnakeMoves
+namespace Miner
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] dimentions = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            string snake = Console.ReadLine();
-            char[,] matrix = new char[dimentions[0], dimentions[1]];
-
-            bool rightToLeft = true;
-            int indexOfLastSymbol = 0;
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[fieldSize, fieldSize];
+            int rowOfMiner = 0;
+            int colOfMiner = 0;
+            List<string> listOfMovements = Console.ReadLine().Split().ToList();
+            int coal = 0;
+            int endRow = 0;
+            int endCol = 0;
+            for (int row = 0; row < fieldSize; row++)
             {
-                if (rightToLeft)
+                char[] rowInput = Console.ReadLine().Split().Select(char.Parse).ToArray();
+                for (int col = 0; col< fieldSize; col++)
                 {
-                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    matrix[row, col] = rowInput[col];
+                    if (matrix[row,col]=='c')
                     {
-                        matrix[row, col] = snake[indexOfLastSymbol++];
-                        if (indexOfLastSymbol==snake.Length)
-                        {
-                            indexOfLastSymbol = 0;
-                        }
+                        coal++;
                     }
-                    rightToLeft = false;
-                }
-                else
-                {
-                    for (int col = matrix.GetLength(1)-1; col >= 0; col--)
+                    if (matrix[row,col]=='e')
                     {
-                        matrix[row, col] = snake[indexOfLastSymbol++];
-                        if (indexOfLastSymbol == snake.Length)
-                        {
-                            indexOfLastSymbol = 0;
-                        }
+                        endRow = row;
+                        endCol = col;
                     }
-                    rightToLeft = true;
+                    if (matrix[row, col] == 's')
+                    {
+                        rowOfMiner = row;
+                        colOfMiner = col;
+                    }
                 }
             }
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+            Queue<string> queue = new Queue<string>(listOfMovements);
+            string shouldIStop = "no";
+            string result = "";
+            for (int i = 0; i < listOfMovements.Count; i++)
+            {    
+                if (queue.Peek() == "up")
                 {
-                    Console.Write(matrix[row,col]);
+                    if (rowOfMiner - 1 >= 0)
+                    {
+                        rowOfMiner -= 1;
+                        if (matrix[rowOfMiner, colOfMiner] == 'c')
+                        {
+                            coal--;
+                            matrix[rowOfMiner, colOfMiner] = '*';
+                            if (coal == 0)
+                            {
+                                result=$"You collected all coals! ({rowOfMiner}, {colOfMiner})";
+                                shouldIStop = "yes";
+                            }
+                        }
+                        if (matrix[rowOfMiner, colOfMiner] == 'e')
+                        {
+                            result=$"Game over! ({rowOfMiner}, {colOfMiner})";
+                            shouldIStop = "yes";
+                        }
+                    }
                 }
-                Console.WriteLine();
+                else if (queue.Peek() == "down")
+                {
+                    if (rowOfMiner + 1 < fieldSize)
+                    {
+                        rowOfMiner += 1;
+                        if (matrix[rowOfMiner, colOfMiner] == 'c')
+                        {
+                            coal--;
+                            matrix[rowOfMiner, colOfMiner] = '*';
+                            if (coal == 0)
+                            {
+                                result = $"You collected all coals! ({rowOfMiner}, {colOfMiner})";
+                                shouldIStop = "yes";
+                            }
+                        }
+                        if (matrix[rowOfMiner, colOfMiner] == 'e')
+                        {
+                            result =$"Game over! ({rowOfMiner}, {colOfMiner})";
+                            shouldIStop = "yes";
+                        }
+                    }
+                }
+                else if (queue.Peek() == "left")
+                {
+                    if (colOfMiner - 1 >= 0)
+                    {
+                        colOfMiner -= 1;
+                        if (matrix[rowOfMiner, colOfMiner] == 'c')
+                        {
+                            coal--;
+                            matrix[rowOfMiner, colOfMiner] = '*';
+                            if (coal == 0)
+                            {
+                                result = $"You collected all coals! ({rowOfMiner}, {colOfMiner})";
+                                shouldIStop = "yes";
+                            }
+                        }
+                        if (matrix[rowOfMiner, colOfMiner] == 'e')
+                        {
+                            result = $"Game over! ({rowOfMiner}, {colOfMiner})";
+                            shouldIStop = "yes";
+                        }
+                    }
+                }
+                else if (queue.Peek() == "right")
+                {
+                    if (colOfMiner + 1 < fieldSize)
+                    {
+                        colOfMiner += 1;
+                        if (matrix[rowOfMiner, colOfMiner] == 'c')
+                        {
+                            coal--;
+                            matrix[rowOfMiner, colOfMiner] = '*';
+                            if (coal == 0)
+                            {
+                                result = $"You collected all coals! ({rowOfMiner}, {colOfMiner})";
+                                shouldIStop = "yes";
+                            }
+                        }
+                        if (matrix[rowOfMiner, colOfMiner] == 'e')
+                        {
+                            result = $"Game over! ({rowOfMiner}, {colOfMiner})";
+                            shouldIStop = "yes";
+                        }
+                    }
+                }
+                if (shouldIStop=="yes")
+                {
+                    break;
+                }
+                queue.Dequeue();
+                
+            }
+            if (shouldIStop == "no")
+            {
+                Console.WriteLine($"{coal} coals left. ({rowOfMiner}, {colOfMiner})");
+            }
+            if (shouldIStop=="yes")
+            {
+                Console.WriteLine(result);
             }
         }
     }
