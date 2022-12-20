78a60a228d0c212bd3d743f02329f17eb7561c49
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:02:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"D:\Stream Files And Directories ex\Stream Files And Directories ex\text.txt"))
+            {
+                int index = 0;
+                string lines = reader.ReadLine();
+                    int counter = 0;
+                    int signs =1;
+                
+                foreach (string line in System.IO.File.ReadLines(@"D:\Stream Files And Directories ex\Stream Files And Directories ex\text.txt"))
+                {
+                    index++;
+                    foreach (var item in line)
+                    {
+                        if ((item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z'))
+                        {
+                            counter++;
+                        }
+                        else if (item >= '0' && item <= '9')
+                        {
+                            counter++;
+                        }
+                        else
+                        {
+                            if (item != ' ')
+                            {
+                                signs++;
+                            }
+                        }
+                    }
+                    Console.WriteLine($"Line {index}: {lines} ({counter})({signs})");
+                    counter = 0;
+                    signs = 0;
+                }
+            }
+        }
+    }
+}
