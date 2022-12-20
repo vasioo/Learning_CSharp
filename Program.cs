2569db30ccca7131ed98909f2e81939bc055156c
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:51:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace VLogger
+namespace EvenTimes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> command = Console.ReadLine().Split().ToList();
-            var vloggerName = command[0];
-            HashSet<string> set = new HashSet<string>();
-            if (command[1]=="joined")
+            double n = double.Parse(Console.ReadLine());
+            HashSet<double> set = new HashSet<double>();
+            List<double> list = new List<double>();
+            for (int i = 0; i < n; i++)
             {
-                set.Add(command[0]);
+                var check = double.Parse(Console.ReadLine());
+                if (set.Contains(check))
+                {
+                    list.Add(check);
+                    list.Add(check);
+                }
+                else
+                {
+                    set.Add(check);
+                }
             }
+            List<double> temp = new List<double>(); 
+            for (int i = 0; i < list.Count; i++)
+            {
+                int counter = 1;
+                for (int z = i+1; z < list.Count; z++)
+                {
+                    if (list[i]==list[z])
+                    {
+                        counter++;
+                    }
+                }
+                if (counter%2==0)
+                {
+                    temp.Add(list[i]);
+                }
+            }
+            Console.WriteLine(String.Join(" ",temp));
         }
     }
 }
