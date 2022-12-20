d485f066b175d8f1c453063329c4652da50eb9ca
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:21:11 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.IO;
-namespace PrintingNames
+
+namespace ListOfPredicates
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] names = Console.ReadLine().Split();
-            Action<string[]> printNames = name
-                => Console.WriteLine(
-                    string.Join(Environment.NewLine,name));
-            printNames(names);
+            int n = int.Parse(Console.ReadLine());
+            List<int> listOfNums = Enumerable.Range(1,n).ToList();     
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+            foreach (var numb in array)
+            {
+                predicates.Add(x => x % numb == 0);
+            }
+            foreach (var number in listOfNums)
+            {
+                bool isDivisible = true;
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+                if (isDivisible)
+                {
+                    Console.Write(number+" ");
+                }
+            }
         }
     }
 }
