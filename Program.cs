9c1ab0cb370bc9cf3db7d419238680719726e05d
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:21:51 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace KnightsOfHonor
+namespace FindEvensAndOds
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Action<string[],string> printKnights = (name,title)
-                => Console.WriteLine(
-                    title+String.Join(Environment.NewLine+title,name));
-            string[] knightNames = Console.ReadLine().Split();
-            printKnights(knightNames,"Sir ");
+            int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string oddEven = Console.ReadLine();
+            List<int> numbers = new List<int>();
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                numbers.Add(i);
+            }
+            Predicate<int> isEven = x
+                 => x % 2 == 0;
+            Predicate<int> isOdd = x
+                => x % 2 == 1;
+            List<int> result;
+            if (oddEven=="odd")
+            {
+                result = numbers.FindAll(isOdd);
+            }
+            else if (oddEven=="even")
+            {
+                result = numbers.FindAll(isEven);
+            }
+            else
+            {
+                result = null;
+            }
+            Console.WriteLine(String.Join(" ", result));
         }
     }
 }
