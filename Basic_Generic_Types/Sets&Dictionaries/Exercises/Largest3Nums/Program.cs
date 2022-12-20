edf7693f01bf738808708631695db956272f90b9
Vasil <vaskoorakov17@gmail.com>
2022-12-20 17:57:13 +0200

    updated the folder

---
+++ Basic_Generic_Types/Sets&Dictionaries/Exercises/Largest3Nums/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest3Numbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            numbers = numbers.OrderByDescending(x => x)
+                .Take(3)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
---
+++ Basic_Generic_Types/Sets&Dictionaries/Exercises/ProductShop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, double>> shopUtilities =new Dictionary<string, Dictionary<string, double>>();
+             string items = Console.ReadLine();
+            while (items!="Revision")
+            {
+                var splitted = items.Split(", ");
+                var shopName = splitted[0];
+                var product = splitted[1];
+                double price = double.Parse(splitted[2]);
+               
+                if (!shopUtilities.ContainsKey(shopName))
+                {
+                    shopUtilities.Add(shopName, new Dictionary<string, double>());
+                }
+                shopUtilities[shopName].Add(product, price);
+                items = Console.ReadLine();
+            }
+            shopUtilities = shopUtilities.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var shop in shopUtilities)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var item in shop.Value)
+                {
+                    Console.WriteLine($"Product: {item.Key}, Price: {(item.Value)}");
+                }
+            }
+        }
+    }
+}
