70a0eba58732f6fc998330e5f1a1edbcb616b69f
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:14:40 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,43 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Delegates
+namespace FilterByAge
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x = int.Parse(Console.ReadLine());
-            int y= int.Parse(Console.ReadLine());
-            string sign = Console.ReadLine();
-            GetMethod(sign);
-            Func<int, int, int> calc = GetMethod(sign);
-            Console.WriteLine(calc(x,y));
-          
-        }
-
-        static int Multiply(int x, int y)
-        {
-            return x * y;
-        }
-        static int Substract(int x, int y)
-        {
-            return x - y;
-        }
-        static int Sum(int x, int y)
-        {
-            return x + y;
-        }
-        static Func<int,int,int> GetMethod(string input)
-        {
-            switch (input)
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, int> saver = new Dictionary<string, int>();
+            for (int i = 0; i < n; i++)
             {
-                case "*": return Multiply;
-                case "+":
-                    return Sum;
-                case "-":
-                    return Substract;
-                default:
-                    return null;
+                string[] people = Console.ReadLine().Split(", ");
+                saver.Add(people[0],Convert.ToInt32(people[1]));
+            }
+            string command = Console.ReadLine();
+            int checker = 0;
+            if (command=="older")
+            {
+                checker = int.Parse(Console.ReadLine());
+                string whatToPrint = Console.ReadLine();
+                foreach (var item in saver)
+                {
+                    if (item.Value>=checker)
+                    {
+                        if (whatToPrint=="name")
+                        {
+                            Console.WriteLine($"{item.Key}");
+                        }
+                        if (whatToPrint == "age")
+                        {
+                            Console.WriteLine($"{item.Value}");
+                        }
+                        if (whatToPrint == "name age")
+                        {
+                            Console.WriteLine($"{item.Key} - {item.Value}");
+                        }
+                    }
+                }
+            }
+            if (command=="younger")
+            {
+                checker = int.Parse(Console.ReadLine());
+                string whatToPrint = Console.ReadLine();
+                foreach (var item in saver)
+                {
+                    if (item.Value < checker)
+                    {    
+                        if (whatToPrint == "name")
+                        {
+                            Console.WriteLine($"{item.Key}");
+                        }
+                        if (whatToPrint == "age")
+                        {
+                            Console.WriteLine($"{item.Value}");
+                        }
+                        if (whatToPrint == "name age")
+                        {
+                            Console.WriteLine($"{item.Key} - {item.Value}");
+                        }
+                    }
+                }
             }
         }
     }
