35af192e1acf4273a0821d3c7835911c76396e7d
Vasil <vaskoorakov17@gmail.com>
2022-12-20 18:29:51 +0200

    added generic exercises

---
+++ Basic_Generic_Types/Generics/Exercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Box<int> box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Items.Add(input);
+            }
+            int[] swapping = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.FirstIndex = swapping[0];
+            box.SecondIndex = swapping[1];
+
+            Console.WriteLine(box);
+        }
+    }
+}
