f5b22b90cdb9aa62013ce6e494dd8c847885b9e5
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:07:07 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.IO;
-namespace ReadOddLines
+
+namespace Streams_Files_And_Directories
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using (StreamWriter writer = new StreamWriter("output.txt"))
+            using (StreamReader reader = new StreamReader
+                (@"directory"))
             {
-              Console.WriteLine("Hey hey");
-                using (StreamReader reader = new StreamReader(@"directory"))
-                {
-                    int index = 0;
-                    var line = reader.ReadLine();
-                    while (line!=null)
-                    {
-                        writer.WriteLine($"{index + 1}. {line}");
-                        index++;
-                        line = reader.ReadLine();
-                    }
-                }
+                string textFromFile = reader.ReadToEnd();
+                Console.WriteLine(textFromFile);
             }
         }
     }
