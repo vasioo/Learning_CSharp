8cbd3e5729d00d117a753d9369d5c3babba376b8
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:15:13 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace FilterByBetter
+namespace FunctionalProgramming
 {
-    class Student
-    {
-        public Student(string name, int age)
-        {
-            this.Name = name;
-            this.Age = age;
-        }
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
     class Program
     {
+        static int PromotionsCountToday = 0;
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>();
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
-            {
-                string[] input = Console.ReadLine().Split(", ");
-                students.Add(new Student(input[0], int.Parse(input[1])));
-            }
-            string filterInput = Console.ReadLine();
-            int ageFilter = int.Parse(Console.ReadLine());
-            string formatInput = Console.ReadLine();
-
-            Func<Student,int, bool> filter = GetFilter(filterInput);
-            students = students.Where(x => filter(x, ageFilter)).ToList();
-            Action<Student> printer = GetPrinter(formatInput);
-            students.ForEach(printer);
+            Console.WriteLine(GetPromotion(100));
         }
-
-        private static Action<Student> GetPrinter(string formatInput)
+        public static decimal GetPromotion(decimal price)
         {
-            switch (formatInput)
+            if (PromotionsCountToday<3)
             {
-                case "name": return s => Console.WriteLine(s.Name);
-                case "age": return s => Console.WriteLine(s.Age);
-                case "name age": return s => Console.WriteLine($"{s.Name} - {s.Age}");
-                default: return null;
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday)
+                {
+                    PromotionsCountToday++;
+                    return price - price * 0.2m;
+                }
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Tuesday)
+                {
+                    PromotionsCountToday++;
+                    return price - price * 0.2m;
+                }
             }
-        }
-
-        private static Func<Student,int, bool> GetFilter(string filterInput)
-        {
-            switch (filterInput)
-            {
-                case "older": return (s, age) => s.Age >= age;
-                case "younger": return (s, age) => s.Age < age;
-                default:
-                    return null;
-            }
+            
+            return price;
         }
     }
 }
