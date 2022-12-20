c8f24313ebfe536bb8202e35580704194cf76ca3
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:20:51 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-namespace PredicateForNames
+using System.IO;
+namespace PrintingNames
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
             string[] names = Console.ReadLine().Split();
-            Func<string,bool> funct = (name)
-                => name.Length <=n;
-            string[] result = names.Where(x => funct(x)).ToArray();
-            Console.WriteLine(String.Join(Environment.NewLine, result)); 
+            Action<string[]> printNames = name
+                => Console.WriteLine(
+                    string.Join(Environment.NewLine,name));
+            printNames(names);
         }
     }
 }
