fd9a4b67143229326dee564e5c4075e2822c1819
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:55:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
-namespace ProductShop
+namespace Largest3Numbs
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Dictionary<string, double>> shopUtilities =new Dictionary<string, Dictionary<string, double>>();
-             string items = Console.ReadLine();
-            while (items!="Revision")
-            {
-                var splitted = items.Split(", ");
-                var shopName = splitted[0];
-                var product = splitted[1];
-                double price = double.Parse(splitted[2]);
-               
-                if (!shopUtilities.ContainsKey(shopName))
-                {
-                    shopUtilities.Add(shopName, new Dictionary<string, double>());
-                }
-                shopUtilities[shopName].Add(product, price);
-                items = Console.ReadLine();
-            }
-            shopUtilities = shopUtilities.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-            foreach (var shop in shopUtilities)
-            {
-                Console.WriteLine($"{shop.Key}->");
-                foreach (var item in shop.Value)
-                {
-                    Console.WriteLine($"Product: {item.Key}, Price: {(item.Value)}");
-                }
-            }
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            numbers = numbers.OrderByDescending(x => x)
+                .Take(3)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ",numbers));
         }
     }
 }
