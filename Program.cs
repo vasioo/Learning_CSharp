cd9f72cd02f1d8470ebd94af2771a13dbc566e52
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:18:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNums = Console.ReadLine().
+                Split().
+                Reverse().
+                Select(int.Parse).
+                ToArray();
+            int n = int.Parse(Console.ReadLine());
+            
+            Func<int, int, bool> isDiv = (x, y) => x % y != 0;
+            List<int> stack = arrayOfNums.Where(x=>isDiv(x,n)).ToList();
+            Console.WriteLine(String.Join(" ",stack));
+        }
+    }
+}
