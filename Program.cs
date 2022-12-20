f4bedd05dfeb328d2e7b2f1f5b1fcde2ac611179
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:29:56 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,91 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace _03_MaximumAndMinimumElement
+namespace _02.BasicQueueOperations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            Stack<int> stack = new Stack<int>();
-            List<string> result = new List<string>();
-            for (int i = 0; i < N; i++)
+            int[] PushPopFind = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            //number of elments into the stack
+            int N = PushPopFind[0];
+            //number of elements to pop from the stack
+            int S = PushPopFind[1];
+            //which element to search for
+            int X = PushPopFind[2];
+            string IsItPresent = "";
+            int[] elementsToBePushed = new int[N];
+            elementsToBePushed = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> stack = new Queue<int>();
+            List<int> list = new List<int>();
+            foreach (var item in elementsToBePushed)
             {
-                int smallestNumber = 0;
-                int biggestNumber = 0;
-                List<int> temp = new List<int>();
-                List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
-                int command =list[0];
-                if (command==1)
-                {
-                    int element = list[1];
-                    stack.Push(element);
-                }
-                if (command == 2&& stack.Count>0)
-                {
-                    stack.Pop();
-                }
-                if (command == 3)
-                {
-                    if (stack.Count == 0)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        foreach (var item in stack)
-                        {
-                            temp.Add(item);
-                        }
-                        biggestNumber = temp[0];
-                        for (int j = 0; j < temp.Count; j++)
-                        {
-                            if (temp[j] >= biggestNumber)
-                            {
-                                biggestNumber = temp[j];
-                            }
-                        }
-                       result.Add(biggestNumber.ToString());
-                    }
-                }
-                if (command == 4)
-                {
-                    if (stack.Count == 0)
-                    {
-                        return;
-                    }
-                    else
-                    {
-                        foreach (var item in stack)
-                        {
-                            temp.Add(item);
-                        }
-                        smallestNumber = temp[0];
-                        for (int j = 0; j < temp.Count; j++)
-                        {
-                            if (temp[j] <= smallestNumber)
-                            {
-                                smallestNumber = temp[j];
-                            }
-                        }
-                        result.Add(smallestNumber.ToString());
-                    }
-                }
-
+                stack.Enqueue(item);
             }
-            string resu = "";
+            for (int i = 0; i < S; i++)
+            {
+                stack.Dequeue();
+            }
             foreach (var item in stack)
             {
-                 resu +=(item+", ");
+                if (item == X)
+                {
+                    IsItPresent = "true";
+                    break;
+                }
             }
-            resu =resu.TrimEnd(' ');
-            result.Add(resu.TrimEnd(','));
-            foreach (var item in result)
+            if (IsItPresent != "true")
             {
-                Console.WriteLine(item);
+                int smallestNumber = 0;
+                if (stack.Count == 0)
+                {
+                    smallestNumber = 0;
+                }
+                else
+                {
+                    foreach (var item in stack)
+                    {
+
+                        list.Add(item);
+                    }
+                    smallestNumber = list[0];
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] <= smallestNumber)
+                        {
+                            smallestNumber = list[i];
+                        }
+                    }
+                }
+                IsItPresent = $"{smallestNumber}";
             }
+            Console.WriteLine(IsItPresent);
         }
-        
     }
 }
