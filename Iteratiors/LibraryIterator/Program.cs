cf56060405a8dc1cb57675bbf209ebe9fb24b84e
Vasil <vaskoorakov17@gmail.com>
2022-12-20 18:10:53 +0200

    added iterators and comparators functionality

---
+++ Iteratiors/LibraryIterator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book()
+            {
+                Title = "Aaa",
+                Year = 1900,
+                Authors = new List<string> { "A" }
+            });
+            books.Add(new Book()
+            {
+                Title = "Bbb",
+                Year = 2000,
+                Authors = new List<string> { "B", "C" }
+            });
+            books.Add(new Book()
+            {
+                Title = "Ccc",
+                Year = 3000,
+                Authors = new List<string> { "A", "B", "C" }
+            });
+            books.Sort(new BookComperator());
+            Library library = new Library(books);
+            foreach (Book book in library)
+            {
+                Console.WriteLine($"{book.Title} ---- {book.Year}");
+            }
+        }
+    }
+}
