56dcd20aa03865493710881e5f3534511867093a
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:05:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.IO;
 
-namespace BaseStreams
+namespace Directories
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MemoryStream stream = new MemoryStream();
-            byte[] buffer = new byte[] { 1, 2, 3, 4, 5 };
-            stream.Write(buffer, 0, buffer.Length);
-            byte[] read = new byte[buffer.Length];
-            stream.Seek(0, SeekOrigin.Begin);
-            stream.Read(read, 0,(int) stream.Length);
-
-            Console.WriteLine(String.Join(" ",read));
+            Directory.CreateDirectory("../../../CoolDir");
+            Directory.Move("../../../CoolDir", "../../../bin/CollDir2");
+            Directory.Delete("../../../bin/CollDir2");
         }
     }
 }
