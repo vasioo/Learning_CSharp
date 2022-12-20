a71ddd3da70a1dc4ec935094d124e87c49000e61
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 18:05:31 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
-namespace Directories
+namespace FStreans
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Directory.CreateDirectory("../../../CoolDir");
-            Directory.Move("../../../CoolDir", "../../../bin/CollDir2");
-            Directory.Delete("../../../bin/CollDir2");
+            //string text = "Pehso";
+            //using (FileStream stream = new FileStream("text.txt",FileMode.OpenOrCreate))
+            //{
+            //    Console.WriteLine(stream.Length);
+            //    stream.Seek(stream.Length, SeekOrigin.Begin);
+            //    byte[] data =Encoding.UTF8.GetBytes(text);
+            //    Console.WriteLine($"{String.Join(",",data)}");
+            //    stream.Write(data,0,data.Length);
+            //}
+            using (FileStream stream = new FileStream("text.txt", FileMode.OpenOrCreate))
+            {
+                byte[] data = new byte[5];
+                while (stream.Read(data,0,data.Length)>0)
+                {
+                    stream.Read(data, 0, data.Length);
+                    string text = Encoding.UTF8.GetString(data);
+                    Console.WriteLine(String.Join(" ", data));
+                    data= new byte[5];
+                }
+            }
         }
     }
 }
