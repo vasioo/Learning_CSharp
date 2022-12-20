6f13284d6b532ab86178a78ed97171030425ccc2
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-20 17:56:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
-namespace HashSets
+using System.Collections.Generic;
+namespace HashSet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<int> set = new HashSet<int>();
+            set.Add(5);  
+            set.Add(2);  
+            set.Add(3);  
+            set.Add(1);  
+            set.Add(52);
+
+            Console.WriteLine(set.Contains(2));
         }
     }
 }
